099f00090796f813f4984159977ccd1d6f9db88f
mattiacoli <mattia.coli@aol.com>
2025-06-18 12:23:21 +0200

    Milestone 2 add cart component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import { useState } from 'react'
 
 function App() {
 
+  const [addedProducts, setAddedProducts] = useState([])
+
   const products = [
     { name: 'Mela', price: 0.5 },
     { name: 'Pane', price: 1.2 },
     { name: 'Latte', price: 1.0 },
     { name: 'Pasta', price: 0.7 },
   ];
+
+  const addProduct = (product) => {
+    const exists = addedProducts.some(item => item.name === product.name)
+
+    if (!exists) {
+
+      const item = {
+        ...product,
+        quantity: 1
+      }
+      setAddedProducts([...addedProducts, item])
+    } else {
+      console.log('Prodotto già presente nel carrello ')
+    }
+  }
 
 
   return (
@@ -20,9 +37,26 @@ function App() {
           <li key={i}>
             <strong>{p.name}</strong>
             <p>prezzo: {p.price} euro</p>
+            <button onClick={() => addProduct(p)}>Aggiungi al carrello</button>
           </li>
         ))}
       </ul>
+
+
+      {addedProducts?.length != 0 &&
+        <div className="cart">
+          <h2>il Tuo Carrello</h2>
+          <ul>
+            {addedProducts.map((item, i) => (
+              <li key={i}>
+                <strong>{item.name}</strong>
+                <p>prezzo: {item.price} euro</p>
+                <p>quantità: {item.quantity}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      }
     </div>
   )
 }
