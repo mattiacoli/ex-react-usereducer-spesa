de432e99650e8e766ed787b242b1bca720abda68
mattiacoli <mattia.coli@aol.com>
2025-06-18 11:15:57 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
+
+
+  return (
+    <>
+      <h1>HI</h1>
+    </>
+  )
+}
+
+export default App
